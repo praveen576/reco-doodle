7c76cdbfea720ff7c5b1efcd075da5ef2b17f7fb
arpit-jalan <arpit.jalan@traveltriangle.com>
2018-03-22 20:01:32 +0530

    initialize rails app

---
+++ reco_doodle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecoDoodle::Application.config.secret_token = 'c2e39769007833dbc92cd0ce025458cc025613699936229665b848b932a7a170fa34e73f49b187a6171dc23e02be765dcca0673227294345743434231361993e'
