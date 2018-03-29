8954f762bf6cabe0eaa25eab8ba9b322777ede0f
praveen-tt <praveen.kumar@traveltriangle.com>
2018-03-29 15:24:11 +0530

    addded seeds data

--- reco_new/db/seeds.rb
+++ reco_new/db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
 user = User.create(email: "admin@reco.com", password: "123456")
 
 role = Role.create(name: "admin")
@@ -15,29 +13,53 @@ role2 = Role.create(name: "traveller")
 
 user.roles << role
 
-destination = Destination.find_or_create_by(name: "Kerala")
-destination2 = Destination.find_or_create_by(name: "Malaysia")
+dest_names = ["Kerala", "Goa", "Himachal", "Sikkim", "Kashmir", "Bali", "Malaysia", "Maldives", "Singapore", "Thailand", "Andaman", "Gujrat"]
 
-activity = Activity.find_or_create_by(name: "Adventure", description: "Looking for some adventtures!!")
-attraction = Attraction.find_or_create_by(name: "Wildlife", description: "Wildlife is fun!!")
-
-activity2 = Activity.find_or_create_by(name: "Entertainment", description: "Looking for some entertainment!!")
-attraction2 = Attraction.find_or_create_by(name: "Nature", description: "Nature is awesome!!")
-
-activity3 = Activity.find_or_create_by(name: "Calm", description: "Looking for calm activity!!")
-attraction3 = Attraction.find_or_create_by(name: "Deserts", description: "Deserts are hot!!")
-
-activity4 = Activity.find_or_create_by(name: "Sports", description: "Looking for some sports!!")
-attraction4 = Attraction.find_or_create_by(name: "Beaches", description: "Beaches are soothing!!")
-
-tag = Tag.create(taggeable_type: "Destination", taggeable_id: Destination.first.id, tag_entity_type: "Activity", tag_entity_id: Activity.first.id)
-
-Activity.all.each_with_index do |activity, index|
-  destination = (index%2 == 0) ? Destination.first : Destination.last
-  Tag.create(taggeable_type: "Destination", taggeable_id: destination.id, tag_entity_type: "Activity", tag_entity_id: activity.id)
+dest_names.each do |dest_name|
+  Destination.find_or_create_by(name: dest_name)  
 end
 
-Attraction.all.each_with_index do |attraction, index|
-  destination = (index%2 == 0) ? Destination.first : Destination.last
-  Tag.create(taggeable_type: "Destination", taggeable_id: destination.id, tag_entity_type: "Attraction", tag_entity_id: attraction.id)
+attraction_names = ["Wildlife", "Hill Station", "Lakes", "Beaches", "Desserts", "Urban Cities", "Historic Places", "Religious"]
+
+attraction_names.each do |att_name|
+  Attraction.find_or_create_by(name: att_name, description: att_name)
+end
+
+activities = ["Romantic", "Adventure", "Relaxing", "Fun", "Health"]
+activities.each do |act_name|
+  Activity.find_or_create_by(name: act_name, description: act_name)
+end
+
+dest_attraction_map = YAML.load_file (Rails.root.join('db', 'dest_attraction.yml').to_s)
+dest_attraction_map.each do |dest_name, attractions|
+  destination = Destination.find_by_name(dest_name)
+  attractions.each do |attraction_name|
+    attraction = Attraction.find_by_name(attraction_name)
+    Tag.create(taggeable_type: "Destination", taggeable_id: destination.id, tag_entity_type: "Attraction", tag_entity_id: attraction.id)    
+  end
+end
+
+dest_activity_map = YAML.load_file (Rails.root.join('db', 'dest_activity_map.yml').to_s)
+dest_activity_map.each do |dest_name, activities_with_weight|
+  destination = Destination.find_by_name(dest_name)
+  activities_with_weight.each do |activity_with_weight|
+    activity = Activity.find_by_name(activity_with_weight.first)
+    Tag.create(taggeable_type: "Destination", taggeable_id: destination.id, tag_entity_type: "Activity", tag_entity_id: activity.id, weight: activity_with_weight.last)    
+  end
+end
+
+
+trip_types = ["couple", "family", "friends", "solo"]
+
+trip_types.each do |tt|
+  TripType.find_or_create_by(name: tt)
+end
+
+dest_trip_type_map = YAML.load_file (Rails.root.join('db', 'dest_trip_type_map.yml').to_s)
+dest_trip_type_map.each do |dest_name, trip_types_with_weight|
+  destination = Destination.find_by_name(dest_name)
+  trip_types_with_weight.each do |trip_type_with_weight|
+    trip_type = TripType.find_by_name(trip_type_with_weight.first)
+    Tag.create(taggeable_type: "Destination", taggeable_id: destination.id, tag_entity_type: "TripType", tag_entity_id: trip_type.id, weight: trip_type_with_weight.last)
+  end
 end
